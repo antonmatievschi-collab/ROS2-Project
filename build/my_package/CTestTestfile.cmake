74c4d1a7880bfda4ba600a417bd28adfa4f21d81
antonmatievschi-collab <antonmatievschi-collab>
2026-02-20 18:40:49 +0100

    Cleanup: removed build artifacts and applied gitignore

--- build/my_package/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sarris/ros2_myproject/my_package
-# Build directory: /home/sarris/ros2_myproject/build/my_package
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
